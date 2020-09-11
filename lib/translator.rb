46773f778a5e9baf3e410b7f3ca4f54a9c0f49e0
Reid Watson <j.watsonreid@gmail.com>
2020-09-11 03:09:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,5 +32,19 @@ end
    
    
 def get_japanese_emoticon(yaml_file,emoticon)   
+  library = {}
+  library = load_library(yaml_file)
   
+  translation = nil
+  library.each do |meaning, languages|
+    if emoticon == library[meaning][:english]
+      translation = meaning
+    end
+  end
+  if translation != nil
+    translation
+  else
+    return "Sorry, that emoticon was not found"
+  end
+end
       
