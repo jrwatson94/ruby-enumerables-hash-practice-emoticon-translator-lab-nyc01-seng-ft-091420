97e1d8b4b64c51f8261c728127fde17e1dae1b0e
Reid Watson <j.watsonreid@gmail.com>
2020-09-11 02:21:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,11 @@ require "pry"
 def load_library(file)
   library = YAML.load_file(lib/emoticons.yml)
   
-  
+  library.each do |meaning,emoticons|
+    library[meaning] => {
+      :english => [emoticons[0]],
+      :japanese => [emoticons[1]]
+    }
+      
 end
   
