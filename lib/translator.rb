32ec8e8de49a5515c166e434bf69daf102519373
Reid Watson <j.watsonreid@gmail.com>
2020-09-11 03:08:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,5 +29,8 @@ def get_english_meaning(yaml_file,emoticon)
     return "Sorry, that emoticon was not found"
   end
 end
-      
+   
+   
+def get_japanese_emoticon(yaml_file,emoticon)   
+  
       
