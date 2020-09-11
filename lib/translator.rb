a18d60b2e2d06e26a0bfbf4bd819050e3bcf945a
Reid Watson <j.watsonreid@gmail.com>
2020-09-11 02:13:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,8 @@
 require "yaml"
+require "pry"
 
-def load_library(path)
-  emoji_hash = {}
-  emoji_hash["get_meaning"] = {}
-  emoji_hash["get_emoticon"] = {}
-  emoji = YAML.load_file(path)
-  emoji.each do |word, emojis|
-    emoji_hash["get_meaning"][emojis[1]] = word
-    emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
-  end
-  emoji_hash
-end
+def load_library(file)
+  library = YAML.load_file(lib/emoticons.yml)
+  
+  
+  
